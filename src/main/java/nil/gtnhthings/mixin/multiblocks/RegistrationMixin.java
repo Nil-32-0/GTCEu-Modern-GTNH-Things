09f3f05ed798f95183fe1c575fdf8bbf932d8229
Ethan Lang <ethanc.lang07@gmail.com>
2026-06-28 20:09:18 -0400

    Modify Steam Grinder structure

---
+++ src/main/java/nil/gtnhthings/mixin/multiblocks/RegistrationMixin.java
@@ -0,0 +1,38 @@
+package nil.gtnhthings.mixin.multiblocks;
+
+import static com.gregtechceu.gtceu.api.pattern.Predicates.blocks;
+import static com.gregtechceu.gtceu.api.pattern.Predicates.frames;
+import static com.gregtechceu.gtceu.common.data.GTMaterials.Bronze;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.gregtechceu.gtceu.api.machine.multiblock.PartAbility;
+import com.gregtechceu.gtceu.api.pattern.FactoryBlockPattern;
+import com.gregtechceu.gtceu.api.pattern.Predicates;
+import com.gregtechceu.gtceu.common.data.GTBlocks;
+import com.gregtechceu.gtceu.common.data.machines.GTMultiMachines;
+import com.gregtechceu.gtceu.utils.memoization.GTMemoizer;
+
+@Mixin(GTMultiMachines.class)
+public class RegistrationMixin {
+    @Inject(method = "init", at = @At("RETURN"), remap = false)
+    private static void init(CallbackInfo info) {
+        GTMultiMachines.STEAM_GRINDER.setPatternFactory(GTMemoizer.memoize(() -> FactoryBlockPattern.start()
+            .aisle("XXX", "FXF", "FXF", "#X#")
+            .aisle("XXX", "X#X", "X#X", "XGX")
+            .aisle("XXX", "FSF", "FXF", "#H#")
+            .where('S', Predicates.controller(blocks(GTMultiMachines.STEAM_GRINDER.getBlock())))
+            .where('#', Predicates.air())
+            .where('F', frames(Bronze))
+            .where('G', blocks(GTBlocks.CASING_BRONZE_GEARBOX.get()))
+            .where('H', Predicates.abilities(PartAbility.STEAM))
+            .where('X', blocks(GTBlocks.CASING_BRONZE_BRICKS.get()).setMinGlobalLimited(14)
+                .or(Predicates.abilities(PartAbility.STEAM_IMPORT_ITEMS).setPreviewCount(1))
+                .or(Predicates.abilities(PartAbility.STEAM_EXPORT_ITEMS).setPreviewCount(1)))
+            .build()
+        ));
+    }
+}
